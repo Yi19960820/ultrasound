90aca50a595400ff8a167209ecf65bb37fedeaa1
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-06-22 19:40:41 -0400

    add boost of 5 to tissue

--- prepare_lr.py
+++ prepare_lr.py
@@ -9,15 +9,17 @@ import tqdm
 LR_DIR = '/data/low-rank/'
 SD_DIR = '/data/sim-data/'
 OUT_DIR = '/data/toy/'
+TISSUE_BOOST = 5
 tissue_names = os.listdir(LR_DIR)
 sd_names = os.listdir(SD_DIR)
+random.shuffle(tissue_names)
 random.shuffle(sd_names)
 
 for i in tqdm.tqdm(range(min(len(tissue_names), len(sd_names)))):
     for x in (1,2):
         for z in (1,2):
             blood = loadmat(os.path.join(SD_DIR, sd_names[i]))['bloodData']
-            blood = blood[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]
+            blood = blood[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]*TISSUE_BOOST
             tissue = loadmat(os.path.join(LR_DIR, tissue_names[i]))['acc3']
             tissue = tissue[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]
             np.savez_compressed(os.path.join(OUT_DIR, f'{i}_x{x}_z{z}'), L=tissue, S=blood)
