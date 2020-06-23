8c8c2b0a570f2104d0f174a7621ed187c01d52e1
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-06-23 16:57:00 -0400

    switch model for eval

--- plot_mat.py
+++ plot_mat.py
@@ -7,7 +7,7 @@ def log_rms(mat):
     logplot = 10*np.log10(meansquare/np.amax(meansquare))
     return logplot
 
-if __name__=='__main__':
+def plot_patches():
     outputs = loadmat('/home/sam/Documents/mats/0.mat')
     D = outputs['D']
     Sp = outputs['Sp']
@@ -20,3 +20,18 @@ if __name__=='__main__':
     ax2.imshow(log_rms(S))
     ax3.imshow(log_rms(Sp))
     plt.show()
+
+def plot_loss():
+    losses1 = np.load('/home/sam/Documents/Res3dC_nocon_sim_Res3dC_LossData_Tr2400_epoch20_lr2.00e-03.npz')
+    losses2 = np.load('/home/sam/Documents/working-20/Res3dC_nocon_sim_Res3dC_LossData_Tr2400_epoch20_lr2.00e-03.npz')
+    lossmeans = np.hstack([losses2['arr_0'], losses1['arr_0']])
+    lossmeans_val = np.hstack([losses2['arr_1'], losses1['arr_1']])
+    plt.semilogy(range(1,41), lossmeans, '-s', label='Train loss', markersize=3)
+    plt.semilogy(range(1,41), lossmeans_val, '-x', label='Validation loss', markersize=3)
+    plt.title('Losses over 40 epochs of training base ResNet')
+    plt.xlabel('Epoch')
+    plt.ylabel('MSE loss')
+    plt.legend()
+    plt.show()
+
+plot_loss()
