8a8a653b1127d8e225a48c55b27cd07b59df0b7f
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-06-24 14:48:37 -0400

    debug

--- prepare_lr.py
+++ prepare_lr.py
@@ -29,7 +29,7 @@ for i in range(min(len(tissue_names), len(sd_names))):
             bw_start = sd_names[i].find('vesselwidth')+12
             print(sd_names[i])
             print(sd_names[i][bw_start:])
-            bw_end = (sd_names[i][bw_start:])[sd_names[i][bw_start:].find('_'):].find('_')
+            bw_end = (sd_names[i][bw_start:])[sd_names[i][bw_start:].find('_'):].find('_')+bw_start
             width_str = sd_names[i][bw_start:bw_end]
             print(width_str)
             width = float(width_str[:width_str.find('_')])
