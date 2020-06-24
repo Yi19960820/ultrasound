af3e90d165fb18872efa0289f6b596426ae4e95b
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-06-24 14:47:21 -0400

    debug

--- prepare_lr.py
+++ prepare_lr.py
@@ -27,6 +27,8 @@ for i in range(min(len(tissue_names), len(sd_names))):
             tissue = tissue[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]
 
             bw_start = sd_names[i].find('vesselwidth')+12
+            print(sd_names[i])
+            print(sd_names[i][bw_start:])
             bw_end = (sd_names[i][bw_start:])[sd_names[i][bw_start:].find('_'):].find('_')
             width_str = sd_names[i][bw_start:bw_end]
             print(width_str)
