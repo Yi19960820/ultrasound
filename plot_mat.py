d664299134c03e7638bde20038e6bfe4c51f8a6f
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-06-25 17:50:21 -0400

    more features in plot_mat

--- plot_mat.py
+++ plot_mat.py
@@ -1,19 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 from scipy.io import loadmat
 import scipy.linalg as la
+import sys
+from copy import copy
 
 def log_rms(mat):
-    meansquare = np.sum(np.abs(mat)**2, axis=2, dtype=float)
+    meansquare = np.sum(np.abs(mat)**2, axis=2, dtype=float)/mat.shape[2]
     # logplot = 10*np.log10(meansquare/np.amax(meansquare))
     logplot = meansquare/np.amax(meansquare)
     return logplot
 
-def plot_patches():
-    outputs = loadmat('/home/sam/Documents/mats/6.mat')
+def plot_column(n):
+    # outputs = loadmat(f'/home/sam/Documents/mats-2-real/{n}.mat')
+    outputs = loadmat(f'/home/sam/Documents/mats/{n}.mat')
+    w = 39
+    col = 10
     D = outputs['D']
     Sp = outputs['Sp']
     S = outputs['S']
+    width = outputs['width'][0][0]
+    width_px = w/.0025*width
+
+    fig, ax = plt.subplots(2, 2, figsize=(9,6))
+    plt.set_cmap('hot')
+    ax[0][1].imshow(np.abs(S[:,col])**2, aspect='auto')
+    ax[0][1].set_title('Column 11 per frame')
+    ax[0][0].imshow(log_rms(S))
+    rect = Rectangle((col, -1), 1, w+1, fill=False, color='green')
+    ax[0][0].add_patch(rect)
+    ax[0][0].set_title('Ground truth S')
+    ax[1][1].imshow(np.abs(Sp[:,col])**2, aspect='auto')
+    ax[1][0].set_title('Reconstructed S')
+    ax[1][0].imshow(log_rms(Sp))
+    rect = Rectangle((col, -1), 1, w+1, fill=False, color='green')
+    ax[1][0].add_patch(rect)
+    ax[1][1].set_title('Column 11 per frame')
+
+    plt.show()
+
+def plot_patches(n, q1, q2):
+    # outputs = loadmat(f'/home/sam/Documents/mats-2-real/{n}.mat')
+    outputs = loadmat(f'/home/sam/Documents/mats/{n}.mat')
+    w = 39
+    D = outputs['D']
+    Sp = outputs['Sp']
+    S = outputs['S']
+    width = outputs['width'][0][0]
+    width_px = w/.0025*width
+
+    if q1==0 and q2==0:
+        angle = 90 + 45
+        x = w - width_px/2*np.cos((angle-90)/np.pi*180)
+        y = w - width_px/2*np.sin((angle-90)/np.pi*180)
+    elif q1==1 and q2==1:
+        angle = -75
+        x = -width_px/2*np.cos(angle/np.pi*180)
+        y = width_px/2*np.sin(angle/np.pi*180)
+
+    bbox = Rectangle((x,y), width_px, 39*1.414, angle=angle, fill=False, color='blue')
+    copies = [copy(bbox) for _ in range(3)]
+
 
     fig, ax = plt.subplots(2,3, figsize=(9,6))
     plt.set_cmap('hot')
@@ -22,18 +70,24 @@ def plot_patches():
 
     ax[0][0].imshow(log_rms(D))
     ax[0][0].set_title('Input')
+    ax[0][0].add_patch(bbox)
 
     ax[0][1].imshow(log_rms(S))
     ax[0][1].set_title('Ground truth S')
+    ax[0][1].add_patch(copies[0])
 
     ax[0][2].imshow(log_rms(Sp))
     ax[0][2].set_title('Reconstructed S')
+    ax[0][2].add_patch(copies[1])
+
 
     ax[1][0].semilogy(range(1, len(svals)+1), svals)
     ax[1][0].set_title('Singular values')
 
     ax[1][1].imshow(log_rms(Drec))
     ax[1][1].set_title('SVT')
+    ax[1][1].add_patch(copies[2])
+
 
     plt.show()
 
@@ -63,5 +117,6 @@ def svt(D,e1, e2=None):
     return S, Drec
 
 if __name__=='__main__':
-    plot_patches()
+    # plot_patches(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
     # plot_loss()
+    plot_column(32)
