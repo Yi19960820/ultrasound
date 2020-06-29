c8146ebc6a53da95e29140a615060d9f69da04f9
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-06-29 17:00:10 -0400

    redo prepare_lr to do SVD

--- prepare_lr.py
+++ prepare_lr.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import random
 import tqdm
+from scipy.linalg import svd
 
 def find_2nd(string, substring):
    return string.find(substring, string.find(substring) + 1)
@@ -12,28 +13,38 @@ def find_2nd(string, substring):
 LR_DIR = '/data/low-rank/'
 SD_DIR = '/data/sim-data-better/'
 OUT_DIR = '/data/toy-real/'
-TISSUE_BOOST = 2
-tissue_names = os.listdir(LR_DIR)
+BLOOD_BOOST = 2
+NSV = 4
 sd_names = os.listdir(SD_DIR)
-random.shuffle(tissue_names)
 random.shuffle(sd_names)
 
-for i in tqdm.tqdm(range(min(len(tissue_names), len(sd_names)))):
+for i in tqdm.tqdm(range(len(sd_names))):
+    mats = loadmat(os.path.join(SD_DIR, sd_names[i]))
+    blood = mats['blood']*BLOOD_BOOST
+    tissue = mats['L']
+    angle = mats['a']
+    width = mats['b']
+
+    n1, n2, n3 = tissue.shape
+    caso = tissue.reshape((n1*n2, n3))
+    U, s, Vh = svd(caso, full_matrices=False)
+    caso_red = U[:,:NSV]@np.diag(s[:NSV])@(Vh[:,:NSV].T)
+    tissue = caso_red.reshape((n1, n2, n3))
     for x in (1,2):
         for z in (1,2):
-            blood = loadmat(os.path.join(SD_DIR, sd_names[i]))['bloodData']
-            blood = blood[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]*TISSUE_BOOST
-            tissue = loadmat(os.path.join(LR_DIR, tissue_names[i]))['acc3']
+            
+            blood = blood[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]
             tissue = tissue[int(39*(z-1)):int(39*z), int(39*(x-1)):int(39*x)]
 
-            bw_start = sd_names[i].find('vesselwidth')+12
-            bw_end = find_2nd(sd_names[i][bw_start:], '_')+bw_start
-            width_str = sd_names[i][bw_start:bw_end]
-            width = float(width_str[:width_str.find('_')])
-            width_unit = width_str[width_str.find('_')+1:]
-            if width_unit=='mm':
-                width *= 0.001
-            elif width_unit=='mum':
-                width *= 1e-6
+            # bw_start = sd_names[i].find('vesselwidth')+12
+            # bw_end = find_2nd(sd_names[i][bw_start:], '_')+bw_start
+            # width_str = sd_names[i][bw_start:bw_end]
+            # width = float(width_str[:width_str.find('_')])
+            # width_unit = width_str[width_str.find('_')+1:]
+            # if width_unit=='mm':
+            #     width *= 0.001
+            # elif width_unit=='mum':
+            #     width *= 1e-6
 
-            np.savez_compressed(os.path.join(OUT_DIR, f'{i}_x{x}_z{z}'), L=tissue, S=blood, width=width)
+            np.savez_compressed(os.path.join(OUT_DIR, f'{i}_x{x}_z{z}'), \
+                L=tissue, S=blood, width=width, angle=angle, nsv=NSV, x=x, z=z)
