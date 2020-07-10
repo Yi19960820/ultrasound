fcbee6a254af0ced023b0ab90fc6a60ca97d6464
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-07-10 16:17:56 -0400

    add ratio print

--- count_data.py
+++ count_data.py
@@ -5,8 +5,12 @@ import os
 if __name__=='__main__':
     datadir = '/data/toy-real-ranked/'
     counts = np.zeros(4)
+    ratios = np.zeros(4)
     for fname in tqdm(os.listdir(datadir)):
         data = np.load(os.path.join(datadir, fname))
         rank = data['nsv']
+        ratio = data['coeff']
         counts[rank-1] += 1
+        ratios[int(ratio*2)-2] += 1
     print(counts)
+    print(ratios)
