5063c7aa73c200648a9563ae5692ec02e765defa
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-07-17 19:39:34 -0400

    implement discriminator

--- UGAN.py
+++ UGAN.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import numpy as np
 from ResNet3dC import Conv3dC, ResBlock3dC
 
+def retrieve_from_full(tensor, inds):
+    flat = tensor.flatten(start_dim=True)
+    output = flat.gather(dim=2, index=inds.flatten(start_dim=2)).view_as(inds)
+    return output
+
 class ConvBlock3dC(nn.Module):
     '''
     Convolutional (encoding) block for the U-Net.
@@ -76,9 +81,23 @@ class DeconvBlock3dC(nn.Module):
             yR = self.conv3(yR, yI)
             return yR, yI
 
-class UGenerator3dC(nn.Module):
+class MaxPool3dC(nn.Module):
+    def __init__(self, w, s, p, d=1):
+        super(MaxPool3dC, self).__init__()
+
+        self.pool = nn.MaxPool3d(w, s, p, d, return_indices=True)
+    
+    def forward(self, xR, xI):
+        x = torch.sqrt(torch.square(xR)+torch.square(xI))
+        _, inds = self.pool(x)
+        yR = retrieve_from_full(xR, inds)
+        yI = retrieve_from_full(xI, inds)
+        return yR, yI
+
+
+class UGenerator(nn.Module):
     def __init__(self, gpu=True):
-        super(UGenerator3dC, self).__init__()
+        super(UGenerator, self).__init__()
         
         c  = [1, 4, 8, 16, 32]
         w1 = [0 ,5, 3,  3,  3]
@@ -104,12 +123,59 @@ class UGenerator3dC(nn.Module):
 
         xR0, xI0 = self.enc0(xRi, xIi)
         xR1, xI1 = self.enc1(xR0, xI0)
-        xR2, xI2 = self.enc0(xR1, xI1)
-        xR3, xI3 = self.enc1(xR2, xI2)
-        xR, xI = self.dec4(xR3, xI3)
-        xR, xI = self.dec5(xR+xR2, xI+xI2)
+        xR2, xI2 = self.enc2(xR1, xI1)
+        # xR3, xI3 = self.enc3(xR2, xI2)
+        # xR, xI = self.dec4(xR3, xI3)
+        xR, xI = self.dec5(xR2, xI2)
         xR, xI = self.dec6(xR+xR1, xI+xI1)
         xR, xI = self.dec7(xR+xR0, xI+xI0)
 
         x=torch.cat((xR,xI),-1)
         return x
+
+class UDiscriminator(nn.Module):
+    def __init__(self, shape, gpu=True):
+        super(UDiscriminator, self).__init__()
+
+        c  = [1, 8, 16, 32, 64]
+        w1 = [0 ,5, 3,  3,  3]
+        w2 = [0 ,5, 3,  3,  3]
+        p1 = [0, 2, 1,  1,  1]
+        p2 = [0, 2, 1,  1,  1]
+
+        d1 = mps(mps(mps(shape[0], 2, 2, 1), 2, 2, 0), 2, 2, 0)
+        d2 = mps(mps(mps(shape[1], 2, 2, 1), 2, 2, 0), 2, 2, 0)
+        d3 = shape[2]
+
+        self.enc0 = ConvBlock3dC(c[0], c[1], (w1[1], w2[1]), (p1[1], p2[1]))
+        self.pool1 = MaxPool3dC((2,2,1), (2,2,1), 1)
+        self.enc2 = ConvBlock3dC(c[1], c[2], (w1[2], w2[2]), (p1[2], p2[2]))
+        self.pool3 = MaxPool3dC((2,2,1), (2,2,1), 0)
+        self.enc4 = ConvBlock3dC(c[2], c[3], (w1[3], w2[3]), (p1[3], p2[3]))
+        self.pool5 = MaxPool3dC((2,2,1), (2,2,1), 0)
+        # self.conv6 = Conv3dC(c[3], c[4], (w1[4], w2[4]), (1,1,1), (p1[4], p2[4]))
+        self.fc6R = nn.Linear(d1*d2*d3, 1, bias=False)
+        self.fc6I = nn.Linear(d1*d2*d3, 1, bias=False)
+        self.tanh = nn.Tanh()   # Using tanh because it applies component-wise to complex numbers
+    
+    def forward(self, x):
+        T2=x.shape[-1]
+        T=int(T2/2)
+        xRi=x[:,:,:,:,0:T]
+        xIi=x[:,:,:,:,T:T2]
+
+        xR, xI = self.enc0(xRi, xIi)
+        xR, xI = self.pool1(xR, xI)
+        xR, xI = self.enc2(xR, xI)
+        xR, xI = self.pool3(xR, xI)
+        xR, xI = self.enc4(xR, xI)
+        xR, xI = self.pool5(xR, xI)
+        xR, xI = self.fc6R(xR), self.fc6I(xI)
+        xR, xI = self.tanh(xR), self.tanh(xI)
+
+        x = torch.cat((xR, xI), -1)
+        return x
+
+# Size of max pool output for dimension
+def mps(dim, w, s, p):
+    return int((dim+2*p-(w-1)-1)/s+1)
