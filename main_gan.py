c9d95b4ea06c328f918281753ad4654e26b0560c
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-07-20 20:01:12 -0400

    change order of processing a bit

--- main_gan.py
+++ main_gan.py
@@ -195,7 +195,8 @@ if __name__=='__main__':
 
                 # Now train the generator to fool the discriminator
                 g_optimizer.zero_grad()
-                gen_loss = adv_loss(fake_out, valid)
+                gen_out = discriminator(fake_batch)
+                gen_loss = adv_loss(gen_out, valid)
                 gen_loss.backward()
                 g_optimizer.step()
 
