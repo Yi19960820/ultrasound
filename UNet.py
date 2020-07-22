86617a9a9dc203ee8f9776c486db32cc9c920900
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-07-21 22:05:46 -0400

    implement UNet

---
+++ UNet.py
@@ -0,0 +1,41 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from UGAN import ConvBlock3dC, DeconvBlock3dC, MaxPool3dC
+
+class UNet(nn.Module):
+    def __init__(self, gpu=True):
+        super(UNet, self).__init__()
+
+        c  = [1, 8, 16, 32, 64]
+        w1 = [0 ,5, 3,  3,  3]
+        w2 = [0 ,5, 3,  3,  3]
+        p1 = [0, 2, 1,  1,  1]
+        p2 = [0, 2, 1,  1,  1]
+
+        self.enc0 = ConvBlock3dC(c[0], c[1], (w1[1],w2[1]), (p1[1],p2[1]))
+        self.enc1 = ConvBlock3dC(c[1], c[2], (w1[2],w2[2]), (p1[2],p2[2]))
+        self.enc2 = ConvBlock3dC(c[2], c[3], (w1[3],w2[3]), (p1[3],p2[3]))
+        self.enc3 = ConvBlock3dC(c[3], c[4], (w1[4],w2[4]), (p1[4],p2[4]))
+        self.dec4 = DeconvBlock3dC(c[4], c[3], (w1[3],w2[3]), (p1[3], p2[3]))
+        self.dec5 = DeconvBlock3dC(c[3], c[2], (w1[2],w2[2]), (p1[2], p2[2]))
+        self.dec6 = DeconvBlock3dC(c[2], c[1], (w1[1],w2[1]), (p1[1], p2[1]))
+        self.dec7 = DeconvBlock3dC(c[1], c[0], (w1[1],w2[1]), (p1[1], p2[1]))
+
+    def forward(self, x):
+        T2=x.shape[-1]
+        T=int(T2/2)
+        xRi=x[:,:,:,:,0:T]
+        xIi=x[:,:,:,:,T:T2]
+
+        xR0, xI0 = self.enc0(xRi, xIi)
+        xR1, xI1 = self.enc1(xR0, xI0)
+        xR2, xI2 = self.enc2(xR1, xI1)
+        xR3, xI3 = self.enc3(xR2, xI2)
+        xR, xI = self.dec4(xR3, xI3)
+        xR, xI = self.dec5(xR+xR2, xI+xI2)
+        xR, xI = self.dec6(xR+xR1, xI+xI1)
+        xR, xI = self.dec7(xR+xR0, xI+xI0)
+
+        x=torch.cat((xR,xI),-1)
+        return x
---
+++ main_unet.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 21:48:02 2018
+Modified on Fri Jun 12 2020 by Sam Ehrenstein
+
+@author: Yi Zhang
+@author: Sam Ehrenstein
+"""
+
+import matplotlib 
+matplotlib.use('Agg')
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+import torch.utils.data as data
+
+import sys
+# sys.path.append('../')
+import os
+
+from DataSet import BigImageDataset
+from UNet import UNet
+from CORONA.classes.Dataset import Converter
+from CORONA.classes.Player import Player
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import datetime
+import pickle
+from tqdm import tqdm
+import yaml
+
+def to_var(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+if __name__=='__main__':
+    """Settings"""
+    """========================================================================="""
+    #Name and choice of training set
+    ProjectName='unet'
+    prefix='sim' #invivo,sim_pm,sim
+    #Load model
+    mfile='/results/multi_rank_1_7_sim_UNet_Model_Tr6000_epoch30_lr2.00e-03.pkl'
+
+    """Network Settings: Remember to change the parameters when you change model!"""
+    gpu=True #if gpu=True, the ResNet will use more parameters
+    #Whether to plot predictions during training and frequency
+    plot=True
+    plotT=1
+    if not plot:
+        plt.ioff()
+    #seed
+    seed=1237
+    torch.manual_seed(seed)
+    #parameters for training
+    TrainInstances = 6000 # Size of training dataset
+    ValInstances   = 800
+    BatchSize      = 40
+    ValBatchSize   = 40
+    num_epochs     = 30
+    m, n, p = (40,40,20)
+    frame=10
+    #directory of datasets
+    d_invivo='/data/Invivo/' 
+    d_simpm='/data/Sim_PM/'
+
+    # Load settings from config file
+    cfg = yaml.load(open('/data/resnet.yaml'))
+    d_sim = cfg['datadir']
+    loadmodel = cfg['loadmodel']
+    if loadmodel=='False':
+        loadmodel=False
+    lr_list = [cfg['lr']]
+    if loadmodel:
+        mfile = cfg['mfile']
+    print(loadmodel)
+    """========================================================================="""
+
+    #Dataset, converter and player
+    data_dir={'invivo':d_invivo,'sim_pm':d_simpm,'sim':d_sim}[prefix]
+    conter=Converter()
+    player=Player()
+    formshow={'pre':'concat','shape':(m,n,p)}
+    formlist=[]
+    for i in range(6):
+        formlist.append(formshow)
+    minloss=np.inf
+    #Logs
+    log=open('/results/%s_%s_UNet_Log_Tr%s_epoch%s_lr%.2e.txt'\
+        %(ProjectName,prefix,TrainInstances,num_epochs,lr_list[0]),'a')
+
+    print('Project Name: %s\n'%ProjectName)
+    log.write('Project Name: %s\n\n'%ProjectName)
+    #Loading data
+    print('Loading phase...')
+    print('----------------')
+    log.write('Loading phase...\n')
+    log.write('----------------\n')
+    shape_dset=(m,n,p*2)    # The last dimension is 2*the number of frames (for real and imaginary)
+    #training
+    train_dataset=BigImageDataset(round(TrainInstances),shape_dset,
+                            train=0,data_dir=data_dir)
+    train_loader=data.DataLoader(train_dataset,batch_size=BatchSize,shuffle=True)
+    #validation
+    val_dataset=BigImageDataset(round(ValInstances),shape_dset,
+                            train=1,data_dir=data_dir, train_size=TrainInstances)
+    val_loader=data.DataLoader(val_dataset,batch_size=ValBatchSize,shuffle=True)
+    print('Finished loading.\n')
+    log.write('Finished loading.\n\n')
+
+    # Training
+    for learning_rate in lr_list:
+        #Construct network
+        print('Configuring network...')
+        log.write('Configuring network...\n')
+        if not loadmodel:
+            net=UNet(gpu)
+        else:
+            if mfile[-3:]=='pkl':
+                net=UNet(gpu)
+                state_dict=torch.load(mfile, map_location='cuda:0')
+                net.load_state_dict(state_dict)
+            else:
+                net=torch.load(mfile)
+
+        if torch.cuda.is_available():
+            net=net.cuda()
+
+        #Loss and optimizer
+        floss=nn.MSELoss()
+        optimizer=torch.optim.Adam(net.parameters(), lr=learning_rate)
+
+        #Array for recording data
+        outputs_S = to_var(torch.zeros([1,1,m,m,p*2]))
+        lossmean_vec=np.zeros((num_epochs,))
+        lossmean_val_vec=np.zeros((num_epochs,))
+        
+        #Training
+        print('Training the model over %d samples, with learning rate %.6f\n'\
+            %(TrainInstances,learning_rate))
+        log.write('Training the model over %d samples, with learning rate %.6f\n\n'\
+            %(TrainInstances,learning_rate))
+        # Run over each epoch
+        for epoch in range(num_epochs):
+            #print time
+            ts=time.time()
+            st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            print('\n'+st)
+            log.write('\n'+st+'\n')
+            
+            loss_val_mean=0
+            loss_mean=0
+            # Train
+            print('Loading and calculating training batches...')
+            log.write('Loading and calculating training batches...\n')
+            starttime=time.time()
+            
+            pbar = tqdm(range(int(TrainInstances/BatchSize)))
+            for i,(_,S,D) in enumerate(train_loader):
+                # set the gradients to zero at the beginning of each epoch
+                optimizer.zero_grad()  
+                for ii in range(BatchSize):
+                    inputs=to_var(D[ii])   # "ii"th picture
+                    targets_S=to_var(S[ii])
+
+                    outputs_S=net(inputs[None,None])  # Forward
+                    loss=floss(outputs_S.squeeze(), targets_S)  # Current loss
+                    batch_loss = loss.item()
+                    loss_mean+=batch_loss
+                    loss.backward()
+                optimizer.step()
+                pbar.set_description("Batch loss: %2.7f" % batch_loss)
+            loss_mean=loss_mean/TrainInstances
+            endtime=time.time()
+            print('Training time is %f'%(endtime-starttime))
+            log.write('Training time is %f\n'%(endtime-starttime))
+            
+            # Validation 
+            print('Loading and calculating validation batches...')
+            log.write('Loading and calculating validation batches...\n')
+            starttime=time.time()
+            with torch.no_grad():
+                for _,(_,Sv,Dv) in enumerate(val_loader): 
+                    for jj in range(BatchSize):
+                        inputsv=to_var(Dv[jj])   # "jj"th picture
+                        targets_Sv=to_var(Sv[jj])
+        
+                        outputs_Sv=net(inputsv[None,None])  # Forward
+                        loss_val=floss(outputs_Sv.squeeze(),targets_Sv)  # Current loss
+                        loss_val_mean+=loss_val.item()
+            loss_val_mean=loss_val_mean/ValInstances
+            endtime=time.time()
+            print('Test time is %f'%(endtime-starttime))
+            log.write('Test time is %f\n'%(endtime-starttime))
+
+            #Save model in each epoch
+            if True or loss_val_mean<minloss:
+                print('saved at [epoch%d/%d]'%(epoch+1,num_epochs))
+                log.write('saved at [epoch%d/%d]\n'\
+                    %(epoch+1,num_epochs))
+                torch.save(net.state_dict(), 
+                        "/results/%s_%s_UNet_Model_Tr%s_epoch%s_lr%.2e.pkl"\
+                        %(ProjectName,prefix,TrainInstances,num_epochs,learning_rate))
+                minloss=min(loss_val_mean,minloss)
+        
+            # Print loss
+            if (epoch + 1)%1==0:    # % 10
+                print('Epoch [%d/%d], Lossmean: %.5e, Validation lossmean: %.5e'\
+                    %(epoch+1,num_epochs,loss_mean,loss_val_mean))
+                log.write('Epoch [%d/%d], Lossmean: %.5e, Validation lossmean: %.5e\n'\
+                    %(epoch+1,num_epochs,loss_mean,loss_val_mean))
+
+                if loss.item() > 100:
+                    print('hitbadrut')
+                    log.write('hitbadrut\n')
+                    break
+            
+            #Observe results
+            if plot and ((epoch+1)%plotT==0 or epoch==0):
+                [xtr,ytr,ptr,xval,yval,pval]=conter.torch2np([D[ii],S[ii],outputs_S,
+                                                            Dv[jj],Sv[jj],outputs_Sv],
+                                                            formlist)
+                # player.plotmat([xtr[:,:,frame],ytr[:,:,frame],ptr[:,:,frame],
+                #             xval[:,:,frame],yval[:,:,frame],pval[:,:,frame]],
+                #                 tit=['xtr','ytr','ptr','xval','yval','pval'],
+                #                 supt='Epoch{%d/%d}'%(epoch+1,num_epochs))
+                plt.pause(0.1)
+            
+            lossmean_vec[epoch]=loss_mean
+            lossmean_val_vec[epoch]=loss_val_mean
+
+            np.savez('/results/%s_%s_UNet_LossData_Tr%s_epoch%s_lr%.2e'\
+                %(ProjectName,prefix,TrainInstances,num_epochs,learning_rate),
+                lossmean_vec,lossmean_val_vec)
+
+        """Save logs, prediction, loss figure, loss data, model and settings """
+        #Graphs
+        #Save the prediction figure
+        [xtr,ytr,ptr,xval,yval,pval]=conter.torch2np([D[ii],S[ii],outputs_S,
+                                                    Dv[jj],Sv[jj],outputs_Sv],
+                                                    formlist)
+        player.plotmat([xtr[:,:,frame],ytr[:,:,frame],ptr[:,:,frame],
+                    xval[:,:,frame],yval[:,:,frame],pval[:,:,frame]],
+                        tit=['xtr','ytr','ptr','xval','yval','pval'],
+                        supt='Epoch{%d/%d}'%(epoch+1,num_epochs),ion=False)
+        plt.savefig('/results/%s_%s_UNet_Pred_Tr%s_epoch%s_lr%.2e.png'\
+                    %(ProjectName,prefix,TrainInstances,
+                    num_epochs,learning_rate),ion=False)
+
+        #MSE
+        fig=plt.figure()
+        epochs_vec=np.arange(0, num_epochs, 1)
+        plt.semilogy(epochs_vec,lossmean_vec,'-*',label='loss')
+        plt.semilogy(epochs_vec,lossmean_val_vec,'-*',label='loss_val')
+        plt.xlabel('epoch')
+        plt.ylabel('Loss')
+        plt.ylim(ymin=0)
+        plt.title("MSE")
+        plt.legend()
+        #Save png, pickle, data of MSE
+        plt.savefig('/results/%s_%s_UNet_LossPng_Tr%s_epoch%s_lr%.2e.png'\
+                    %(ProjectName,prefix,TrainInstances,num_epochs,learning_rate))
+        pickle.dump(fig,open("/results/%s_%s_UNet_LossFig_Tr%s_epoch%s_lr%.2e.fig.pickle"\
+                            %(ProjectName,prefix,TrainInstances,
+                            num_epochs,learning_rate),'wb'))
+        np.savez('/results/%s_%s_UNet_LossData_Tr%s_epoch%s_lr%.2e'\
+                %(ProjectName,prefix,TrainInstances,num_epochs,learning_rate),
+                lossmean_vec,lossmean_val_vec)
+        
+        #Save settings of training
+        params={'ProjectName':ProjectName,
+                'prefix':prefix,
+                'mfile':mfile if loadmodel else None,
+                'gpu':gpu,
+                'data_dir':data_dir,
+                'shape':shape_dset,
+                'lr_list':lr_list,
+                'TrainInstances':TrainInstances,
+                'ValInstances':ValInstances,
+                'BatchSize':BatchSize,
+                'ValBatchSize':ValBatchSize,
+                'num_epochs':num_epochs,
+                'frame':frame}
+        file=open('/results/%s_%s_UNet_Settings_Tr%s_epoch%s_lr%.2e.txt'\
+                %(ProjectName,prefix,TrainInstances,num_epochs,learning_rate),'w')
+        file.write('Training Settings:\n\t')
+        for k,v in params.items():
+            file.write(k+'='+str(v)+'\n')
+            file.write('\t')
+        file.close()
+        
+        #Print min loss
+        print('\nmin Loss=%.3e'%minloss)
+        log.write('\nmin Loss=%.3e'%minloss)
+        log.close()
