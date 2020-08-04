64df333fc71411539e51414ca35ede8537e286be
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-08-04 17:05:22 -0400

    axis

--- prepare_lr.py
+++ prepare_lr.py
@@ -11,7 +11,7 @@ import yaml
 def add_padding(arr, length):
     n1, n2, n3 = arr.shape
     padding = np.ones((n1, n2, length))*np.mean(arr)
-    return np.concatenate(arr, padding)
+    return np.concatenate(arr, padding, axis=2)
 
 def find_2nd(string, substring):
     '''
